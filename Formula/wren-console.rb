c4617ab5923d610c9aa399e2033ffe3410d65d9c
Josh Goebel <me@joshgoebel.com>
2021-05-31 20:12:28 -0400

    build from tagged release of wren-essentials

--- Formula/wren-console.rb
+++ Formula/wren-console.rb
@@ -5,17 +5,16 @@ class WrenConsole < Formula
   sha256 "5af9e6383606fec4429dcce88c6643025a2363c0324906a1661d7f6848e204b8"
   license "MIT"
 
-  depends_on "git" => [:build]
+  resource "wren-essentials" do
+    url "https://github.com/joshgoebel/wren-essentials/archive/v0.1.0.tar.gz"
+    sha256 "2192f3c4b8c039895582d87577a473a4eb5f84db827d914d611bb69ec334068a"
+  end
 
   def install
-    on_macos do
-      system "git", "clone", "https://github.com/joshgoebel/wren-essentials", "deps/wren-essentials"
-      system "make", "-C", "projects/make.mac"
-    end
-    on_linux do
-      system "git", "clone", "https://github.com/joshgoebel/wren-essentials", "deps/wren-essentials"
-      system "make", "-C", "projects/make"
-    end
+    project_dir = "projects/make.mac"
+    on_linux { project_dir = "projects/make" }
+    resource("wren-essentials").stage buildpath/"deps/wren-essentials"
+    system "make", "-C", project_dir
     bin.install "bin/wrenc"
     pkgshare.install "example"
   end
