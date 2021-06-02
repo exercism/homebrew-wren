165e4dadfe15c15eb006339331a71a821424ae13
Josh Goebel <hello@joshgoebel.com>
2021-06-02 13:44:52 +0000

    wren-console: add 0.2.90 bottle.

--- Formula/wren-console.rb
+++ Formula/wren-console.rb
@@ -5,6 +5,13 @@ class WrenConsole < Formula
   sha256 "5af9e6383606fec4429dcce88c6643025a2363c0324906a1661d7f6848e204b8"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/exercism/homebrew-wren/releases/download/wren-console-0.2.90"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "e3c2367ad628788fdea73265ed82ca618051301a9385aa4c3cea4a42a4b6a1c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "07bb9b746b03c1339ccf5b5715162a9d03429ca7ec3731c318d22b403b6c7e49"
+  end
+
   resource "wren-essentials" do
     url "https://github.com/joshgoebel/wren-essentials/archive/v0.1.0.tar.gz"
     sha256 "2192f3c4b8c039895582d87577a473a4eb5f84db827d914d611bb69ec334068a"
